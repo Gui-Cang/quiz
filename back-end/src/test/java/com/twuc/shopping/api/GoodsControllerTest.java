160dcef96afb24aac21353d9230586b48f21dfe2
Gui-Cang <821192514@qq.com>
2020-09-25 16:20:53 +0800

    [dingk] fix: modified the contentType in the add test .

--- back-end/src/test/java/com/twuc/shopping/api/GoodsControllerTest.java
+++ back-end/src/test/java/com/twuc/shopping/api/GoodsControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -52,7 +53,8 @@ class GoodsControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         String added = objectMapper.writeValueAsString(goods);
 
-        mockMvc.perform(post("/goods").content(added)).andExpect(status().isCreated());
+        mockMvc.perform(post("/goods").content(added).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
         List<GoodsDto> all = goodsRepository.findAll();
         GoodsDto goodsDto = all.get(0);
 
