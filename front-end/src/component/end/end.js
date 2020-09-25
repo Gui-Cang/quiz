3124fd40736d9983dc525bf14d322fd346454e92
Gui-Cang <821192514@qq.com>
2020-09-25 17:59:54 +0800

    [dingke] feat: do the front end but fail.

---
+++ front-end/src/component/end/end.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './end.css';
+
+class end extends React.Component {
+    render() {
+        return(
+            <div className = "end">
+                <p>TW Mall @2018 Created by Forcheng</p>
+            </div>
+        )
+    }
+}
+
+export default end;
---
+++ front-end/src/component/good/product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './product.css';
+
+class product extends React.Component {
+    render() {
+        return(
+            <div className = "good">
+                <button className = "add">+</button>
+            </div>
+        )
+    }
+}
+export default product;
